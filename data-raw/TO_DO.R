7e93a38281dcb912f61a3d9feaa6d58c6ca3d4ad
Auler-J <auler.jennifer@gmail.com>
2022-09-26 11:44:37 -0300

    add things to do

--- data-raw/TO_DO.R
+++ data-raw/TO_DO.R
@@ -7,3 +7,4 @@
 browseURL("https://r-pkgs.org/Workflow101.html#naming")
 # we chose evoled as a possible name
 # 4) insert input check in all functions. In WFDriftSim line 102 throws an error.
+# 5) restore old par() on exit of functions that plot
