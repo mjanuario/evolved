65fe9b4923f6c86295fb74f51b49627399d8586f
Auler-J <auler.jennifer@gmail.com>
2022-09-28 20:27:00 -0300

    add things to do

--- data-raw/TO_DO.R
+++ data-raw/TO_DO.R
@@ -7,4 +7,7 @@
 browseURL("https://r-pkgs.org/Workflow101.html#naming")
 # we chose evoled as a possible name
 # 4) insert input check in all functions. In WFDriftSim line 102 throws an error.
-# 5) restore old par() on exit of functions that plot. Check with_par() and on.exit() function for this
+# 5) add histogram at the end of WFDriftSim
+# 6) coment better function NatSel
+# 7) animate natsel pannels (all, or choose one)
+# 8) restore old par() on exit of functions that plot. Check with_par() and on.exit() function for this
