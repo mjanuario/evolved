4212773f9264e4e2a88882fbc777e044b2e1f418
mjanuario <januario@umich.edu>
2023-01-16 20:08:44 -0500

    added countSeqDiff w/ documentation

---
+++ R/countSeqDiffs.R
@@ -0,0 +1,50 @@
+#' @importFrom   base unlist
+#' @importFrom  base strsplit
+NULL 
+
+#' Counting protein sequence differences
+#'
+#' \code{countSeqDiffs} counts the number of protein differences among two sequences of proteins within the same "ProteinSet" object.
+#'
+#' @param x A "ProteinSet" object containing proteins from \code{taxon1} 
+#' and \code{taxon2}.
+#' @param taxon1 A character giving the common name of the first species that 
+#' will be compared
+#' @param taxon2 A character giving the common name of the second species that 
+#' will be compared
+#' 
+#' @return A integer giving the number of protein differences between 
+#' \code{taxon1} and \code{taxon2}.
+#' 
+#' @export countSeqDiffs
+#' 
+#' @author Matheus Januario, Dan Rabosky, Jennifer Auler
+#' 
+#' @examples
+#' countSeqDiffs(cytOxidase, "human", "chimpanzee")
+#' 
+#' countSeqDiffs(cytOxidase, "human", "cnidaria")
+#' 
+#' countSeqDiffs(cytOxidase, "chimpanzee", "cnidaria")
+#' 
+countSeqDiffs <- function(x, taxon1, taxon2){
+  if(class(x) != "ProteinSet"){
+    stop("object \"x\" must be from \"ProteinSet\" class")
+  }
+  
+  if(sum(c(taxon1, taxon2) %in% names(x))<2){
+    stop("Both \"taxon1\" and \"taxon2\" must be in \"x\"")
+  }
+  
+  if(class(taxon1)!="character"){
+    stop("\"taxon1\" must be a character")
+  }
+  
+  if(class(taxon2)!="character"){
+    stop("\"taxon2\" must be a character")
+  }
+  
+  x1 <- base::unlist(base::strsplit(x[taxon1], split=""))
+  x2 <- base::unlist(base::strsplit(x[taxon2], split=""))
+  return(sum(x1 != x2))
+}
