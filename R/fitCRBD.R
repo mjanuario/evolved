3bf24388ba28f7a7ad9dedf2bfca38cceb92813a
mjanuario <januario@umich.edu>
2023-02-13 15:35:32 -0500

    Am commiting this separately as am unsure this should be done

---
+++ R/fitCRBD.R
@@ -0,0 +1,101 @@
+#' Fitting a constant-rate birth-death process to a phylogeny
+#'
+#' \code{fitCRBD} fits a constant-rate birth-death process to a phylogeny in the
+#'  format of \code{ape} package's \code{phylo} object. Optimization is based on
+#'   likelihood functions made with \code{diversitree}. This function is 
+#'   basically a wrapper for the \code{diversitree}'s \code{make.bd} function.
+#'
+#' @param phy A \code{phylo} object, following terminology from package 
+#' \code{ape}, in which function will operate.
+#' @param nopt Number of optimizations that will be tried by function.
+#' @param lmin Lower bound for optimization. Default value is 0.001.
+#' @param lmax Upper bound for optimization. Default value is 5.
+#' @param MAXBAD Maximum number of unsuccessful optimization attempts. Default 
+#' value is 200
+#' 
+#' @details see help page from \code{diversitree::make.bd} and 
+#' \code{stats::optim}
+#' 
+#' @return A \code{numeric} with the best estimates of speciation \code{S} 
+#' and extinction \code{E} rates.
+#' 
+#' @export simulateTree
+#' 
+#' @references 
+#' 
+#' Paradis, E. (2012). Analysis of Phylogenetics and Evolution with R (Vol. 2).
+#'  New York: Springer.
+#' 
+#' Popescu, A. A., Huber, K. T., & Paradis, E. (2012). ape 3.0: New tools for 
+#'  distance-based phylogenetics and evolutionary analysis in R. Bioinformatics,
+#'   28(11), 1536-1537.
+#'   
+#'  FitzJohn, R. G. (2010). Analysing diversification with diversitree. 
+#'   R Packag. ver, 9-2.
+#'   
+#'  FitzJohn, R. G. (2012). Diversitree: comparative phylogenetic analyses of 
+#'  diversification in R. Methods in Ecology and Evolution, 3(6), 1084-1092.
+#' 
+#' @author Daniel Rabosky, Matheus Januario, Jennifer Auler
+#' 
+#' @examples
+#' 
+#' S <- 0.1
+#' E <- 0.1
+#' set.seed(1)
+#' phy <- simulateTree(pars = c(S, E), max.taxa = 30)
+#' fitCRBD(phy)
+#' 
+fitCRBD <- function(phy, nopt=5, lmin=0.001, lmax=5.0, MAXBAD = 200){
+  
+  if (length(phy$tip.label) < 3){
+    pars <- c(0.0001,0)
+    names(pars) <- c("S", "E")
+    return(pars)
+  }
+  
+  fx <- diversitree::make.bd(phy)
+  
+  for (i in 1:nopt){
+    
+    lam <- runif(1, 0, 0.5)	
+    E <- lam * runif(1, 0, 1)
+    
+    badcount <- 0
+    
+    resx <- try(stats::optim(c(lam, E) ,fx, method='L-BFGS-B', control=list(maxit=1000, fnscale=-1), lower=lmin, upper=lmax), silent=T)
+    while (class(resx) == 'try-error'){
+      
+      lam <- runif(1, 0, 0.5)	
+      E <- lam * runif(1, 0, 1)
+      
+      resx <- try(stats::optim(c(lam, E) , fx, method='L-BFGS-B', control=list(maxit=1000, fnscale=-1), lower=lmin, upper=lmax), silent=T);
+      
+      badcount <- badcount + 1;
+      if (badcount > MAXBAD){
+        stop("Too many fails in fitDiversitree\n")
+      }
+    }
+    
+    if (i == 1){
+      best <- resx
+    }else{
+      if (best$value < resx$value){
+        best <- resx
+      }
+    }
+    
+  }
+  
+  fres <- list(pars=best$par, loglik=best$value)
+  fres$AIC <- -2*fres$loglik + 2*length(diversitree::argnames(fx))
+  fres$counts <- best$counts
+  #fres$like_function <- fx
+  fres$convergence <- best$convergence
+  fres$message <- best$message
+  
+  pars <- fres$pars
+  names(pars) <- c("S", "E")
+  
+  return(pars)
+}
---
+++ R_raw/R_functions_lab10_cleaned.R
@@ -0,0 +1,62 @@
+#' Plotting the whale phylogeny and coloring its clades
+#'
+#' \code{plot_painted_whales} plots the phylogeny from Steeman et al (2011), coloring the Dolphins (Delphinidae), porpoises (Phocoenidae), the Mysticetes, the baleen whales (Balaenopteridae), and the Beaked whales (Ziphiidae).
+#'
+#'#' @param phy A \code{phylo} object, that must be the same as the \code{BAMMtools::whales} bject
+#' @param show.legend Logical indicating if clade elgend should be shown.
+#' @param .. other arguments to be passed to \code{phytools::plotSimmap}
+#' 
+#' @details see help page from \code{phytools::plotSimmap}
+#' 
+#' @return A colored phylogeny plot.
+#' 
+#' @export plot_painted_whales
+#' 
+#' @references 
+#' 
+#'  Steeman, M. E., Hebsgaard, M. B., Fordyce, R. E., Ho, S. Y., Rabosky, D. L., Nielsen, R., ... & Willerslev, E. (2009). Radiation of extant cetaceans driven by restructuring of the oceans. Systematic biology, 58(6), 573-585.
+#' 
+#'  
+#' @author Matheus Januario, Jennifer Auler
+#' 
+#' @examples
+#' 
+#' 
+plot_painted_whales<-function(phy, show.legend=TRUE, direction="rightwards", ...){
+  
+  #
+  #data(BAMMtools::whales)
+  #if(all.equal(phy,whales)){
+  #  stop("your inputted phylo is not the whale phylogeny from the \"BAMMtools\" package")
+  #}
+  
+  painted<-phytools::paintSubTree(phy,89,"Other mysticetes","1")
+  painted<-phytools::paintSubTree(painted,109,"Beaked whales", "0")
+  painted<-phytools::paintSubTree(painted,134,"Belugas and narwhals","0")
+  painted<-phytools::paintSubTree(painted,135,"Porpoises")
+  painted<-phytools::paintSubTree(painted,94,"Baleen whales",)
+  painted<-phytools::paintSubTree(painted,140,"Dolphins")
+  
+  phytools::plotSimmap(painted,lwd=3)
+  
+  if(show.legend){
+    if(direction=="rightwards"){
+      leg.placement="topleft"
+    }else if(direction=="leftwards"){
+      leg.placement="topright"
+    }else{
+      stop("direction should be equal to rightwards OR leftwards")
+    }
+    
+    legend(x=leg.placement,legend=c(
+      "other odontocetes",
+      "Baleen whales",
+      "Beaked whales",
+      "Belugas and narwhals",
+      "Dolphins",
+      "Other mysticetes","Porpoises"
+    ),
+    pch=21,pt.cex=1.5,pt.bg=c("black","#DF536B","#61D04F","#2297E6","#28E2E5","#CD0BBC","#F5C710"),bty="n") 
+  }
+  
+}
