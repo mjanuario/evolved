8f162e62f2e98e68a10b33fa2a20a05940402760
Jennifer Auler <auler.jennifer@gmail.com>
2024-01-10 23:32:19 -0300

    things to do in the vignettes

--- data-raw/TO_DO.R
+++ data-raw/TO_DO.R
@@ -25,8 +25,22 @@ browseURL("https://r-pkgs.org/Workflow101.html#naming") # - DAN
 # Check licensing of included files
 # Review https://github.com/DavisVaughan/extrachecks 
  
-# 5) fix error "figure margins are too large" in clocks and rocks vignette. I've 
-# silenced the code chunk to pass all checks. (eval=FALSE)
+# 5) things to fix in vignettes:
+ 
+# pop gen:
+#   WFDriftSim plot
+# NatSelSim plot
+# explain what this means ## AA Aa aa 
+## 20 18 18
+
+#clocks and rocks
+# head(cytOxidase) excedes output chunk
+# plotProteinSeq "cnidaria" is partially blank. Also should ajust par in function to be able to see the longest clade
+
+#phylos
+# plot(whale_phylo,cex = 0.5) ajust par to be able to see text and eliminate exceding white space.
+# plotPaintedWhales() plot
+
 
 #6) consistency check of arguments used across several functions (e.g. nGen, Ngens, NGens). nGen and nSim ok
 
