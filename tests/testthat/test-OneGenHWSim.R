5cb2164e8caa9e59d90809f8c6f167e5f08af402
Matheus Januario <14234924+mjanuario@users.noreply.github.com>
2025-07-03 15:13:52 -0400

    Del dir (paper branch)

--- tests/testthat/test-OneGenHWSim.R
+++
@@ -1,3 +0,0 @@
-test_that("fixed allel", {
-  expect_equal(OneGenHWSim(p=1)[,1], rep(50, 100))
-})
--- tests/testthat/test-WFDriftSim.R
+++
@@ -1,3 +0,0 @@
-test_that("allel is fix", {
-  expect_equal(WFDriftSim(N = 5, n.gen = 30, p0=1, plot.type = "none", print.data = TRUE)[,30],1)
-})
